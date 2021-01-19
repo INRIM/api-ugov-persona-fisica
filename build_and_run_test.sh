32aa9eb21364861e6074c592eb6b05f21f3c4100
Alessio Gerace <a.gerace@inrim.it>
2021-01-19 15:38:48 +0100

    [FIX] port for preprod env

--- build_and_run_test.sh
+++ build_and_run_test.sh
@@ -2,4 +2,4 @@
 docker stop ugovws
 docker container rm ugovws
 docker build -t python3.8.ugovws .
-docker run --env WEB_CONCURRENCY=1 --restart always -d --name ugovws -p 8022:80 -v $(pwd)/app:/app python3.8.ugovws /start.sh
+docker run --env WEB_CONCURRENCY=1 --restart always -d --name ugovws -p 9022:80 -v $(pwd)/app:/app python3.8.ugovws /start.sh
