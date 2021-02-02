a4cd17bde1f49bcac2b1a5da7c116e351fe73d7b
Alessio Gerace <a.gerace@inrim.it>
2021-02-02 21:13:07 +0100

    [FIX] prject config

--- clean.sh
+++ clean.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-read -p "Are you sure? [y/N]" -n 1 -r
-echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
 docker rm -fv ugovws-api-pf_ugovws-pf
 docker rmi python3.8.ugovws
-fi
